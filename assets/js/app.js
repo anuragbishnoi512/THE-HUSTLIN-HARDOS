4e8fe0c80d107cd600ca12b1e0f7b5d991545edc
anurag-512 <anuragsharan51@gmail.com>
2024-06-27 15:48:28 +0530

    slider created

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,51 @@ menuicon.addEventListener("click", function () {
     menulist.classList.toggle("showlist");
     menuicon.classList.toggle("fixed1")
 });
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+$('.slider1').slick({
+    dots: false,
+    infinite: true,
+    prevArrow: ".prev1",
+    nextArrow: ".next1",
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
